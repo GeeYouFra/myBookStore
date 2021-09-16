1632e2f40de3fc15a7d13c46a05a24cc24190fd8
ruskollin <giovani.francisco@myy.haaga-helia.fi>
2021-09-16 20:29:37 +0300

    First commit

---
+++ src/main/java/com/example/Bookstore/web/BookController.java
@@ -0,0 +1,19 @@
+package com.example.Bookstore.web;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class BookController {
+
+	@RequestMapping("/index")
+	public String showIndex(@RequestParam(value = "name") String name, Model model) {
+		return "index";
+	}
+
+}
