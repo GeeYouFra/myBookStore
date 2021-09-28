3ace2ca6537506fd475dbc3586f3192fd1fecfe1
Giovani Francisco <giovani.francisco@myy.haaga-helia.fi>
2021-09-28 18:52:51 +0300

    Bookstore REST

--- src/main/java/com/example/Bookstore/web/BookController.java
+++ src/main/java/com/example/Bookstore/web/BookController.java
@@ -1,5 +1,6 @@
 package com.example.Bookstore.web;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.Bookstore.domain.Book;
 import com.example.Bookstore.domain.BookRepository;
@@ -22,11 +23,21 @@ public class BookController {
 
 	@Autowired
 	private CategoryRepository catrepository;
-	
+
 	@RequestMapping(value = { "/", "/booklist" })
 	public String showIndex(Model model) {
 		model.addAttribute("books", repository.findAll());
 		return "booklist";
+	}
+
+	@RequestMapping(value = "/books", method = RequestMethod.GET)
+	public @ResponseBody List<Book> bookListRest() {
+		return (List<Book>) repository.findAll();
+	}
+
+	@RequestMapping(value = "/book/{id}", method = RequestMethod.GET)
+	public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {
+		return repository.findById(bookId);
 	}
 
 	@RequestMapping(value = "/add")
@@ -42,14 +53,14 @@ public class BookController {
 		model.addAttribute("book", new Book());
 		return "redirect:booklist";
 	}
-	
+
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
-    public String deleteStudent(@PathVariable("id") Long bookId, Model model) {
-    	repository.deleteById(bookId);
-    	//TWO DOTS MEAN THAT NOW NEEDS TO GO ONE LEVEL UP IN
-    	//HIERARCHY. ENDPOINT BASE IS /
-        return "redirect:../booklist";
-    } 
+	public String deleteStudent(@PathVariable("id") Long bookId, Model model) {
+		repository.deleteById(bookId);
+		// TWO DOTS MEAN THAT NOW NEEDS TO GO ONE LEVEL UP IN
+		// HIERARCHY. ENDPOINT BASE IS /
+		return "redirect:../booklist";
+	}
 
 	@RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
 	public String editBook(@PathVariable("id") Long bookId, Model model) {
